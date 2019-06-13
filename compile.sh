8ec89f51e9fb69efcfe48291a9aa2e14031f2e3b
John Stilley <1831479+theJollySin@users.noreply.github.com>
2019-06-13 13:37:58 -0400

    #17 flushing out draft zero of the compile script

--- compile.sh
+++ compile.sh
@@ -8,25 +8,37 @@
 # This script is not *necessary* to run the client, though it is preferable to run this script
 # before a release.
 
+# CONFIG
+JS_VERSION="v20190513"
+CSS_VERSION="v1.5.0"
+
+# remove any old compiled versions
 mkdir compiled/css -p
 
-# TODO: wrap this in a test for existence of JS compiler
-mkdir -p js_compiler
-pushd js_compiler
-wget https://dl.google.com/closure-compiler/compiler-20190513.tar.gz
-tar -xvzf compiler-20190513.tar.gz
-mv closure-compiler-v20190513.jar ..
-popd
-rm -rf compiler
-
-java -jar closure-compiler-v20190513.jar js/*.js --js_output_file=compiled/main.js
-
-# If it is not already present, grab the Google closure CSS minifier
-if [ ! -f closure-stylesheets.v1.5.0.jar]
+# If it is not already present, grab the Google closure JS minifier.
+if [ ! -f closure-compiler-${JS_VERSION}.jar]
 then
-	wget https://github.com/google/closure-stylesheets/releases/download/v1.5.0/closure-stylesheets.jar
-	mv closure-stylesheets.jar closure-stylesheets.v1.5.0.jar
+	mkdir -p js_compiler
+	pushd js_compiler
+	wget https://dl.google.com/closure-compiler/compiler-${JS_VERSION}.tar.gz
+	tar -xvzf compiler-${JS_VERSION}.tar.gz
+	mv closure-compiler-${JS_VERSION}.jar ..
+	popd
+	rm -rf compiler
 fi
-java -jar closure-stylesheets.v1.5.0.jar css/*.css -o compiled/css/main.css
 
+# Compile the JS and move it into place.
+java -jar closure-compiler-${JS_VERSION}.jar js/*.js --js_output_file=compiled/main.js
+
+# If it is not already present, grab the Google closure CSS minifier.
+if [ ! -f closure-stylesheets.${CSS_VERSION}.jar]
+then
+	wget https://github.com/google/closure-stylesheets/releases/download/${CSS_VERSION}/closure-stylesheets.jar
+	mv closure-stylesheets.jar closure-stylesheets.${CSS_VERSION}.jar
+fi
+
+# Compile the CSS and move it into place.
+java -jar closure-stylesheets.${CSS_VERSION}.jar css/*.css -o compiled/css/main.css
+
+# Update the HTML to work with compiled resources and move it into place.
 sed '/JAVASCRIPT/c\  <script src="main.js" type="text/javascript">' web_client.html > compiled/web_client.html
